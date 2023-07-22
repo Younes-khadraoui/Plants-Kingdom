4098d26b490457a41a519bb1b1b7744cd0669875
Younes-khadraoui <younes.khadraoui.pro@gmail.com>
2023-07-22 17:49:36 +0100

    small changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div className="p-10 text-white flex justify-center items-center font-poppins text-lg bg-gradient-to-br from-[#A2A2A2] to-white">
-      <div className="flex">
-        <p>Created By Younes Khadraoui</p> <span className="lg:opacity-0">|</span>
+    <div className="p-10 text-black flex justify-center items-center font-poppins text-lg bg-gradient-to-br from-[#A2A2A2] to-white">
+      <div className="flex gap-1 opacity-60">
+        <p >Created By Younes Khadraoui</p> <span className="opacity-0 lg:opacity-100">|</span>
         <p>© 2022 All rights reserved</p>
       </div>
     </div>
